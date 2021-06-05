f0b96f059cf8432f4bfd016c9b3e419600f58b74
Mahesh Hegde <net.mahesh29@gmail.com>
2021-06-05 11:39:39 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,125 @@
+## reddit-rip
+
+It's a simple image downloader CLI tool for reddit
+
+It downloads images from URLs that look like images
+
+It has a few configurable options as well
+
+```
+Usage: reddit-rip <options> <r/subreddit>
+  -after string
+        Get posts after the given ID
+  -d    DryRun i.e just print urls and names
+  -folder string
+        Target folder name
+  -help
+        Show this help message
+  -l int
+        Number of entries to fetch in one API request (devel) (default 100)
+  -max int
+        Max number of files to download (+ve), -1 for no limit (default -1)
+  -max-size int
+        Max size of media file in KB, -1 for no limit (default -1)
+  -max-storage int
+        Data usage limit in MB, -1 for no limit (default -1)
+  -min-karma int
+        Minimum Karma of the post
+  -sort string
+        Sort: best|hot|new|rising|top-<all|year|month|week|day>
+  -useragent string
+        UserAgent string (default "reddit-dl / Command Line Tool For Linux")
+  -v    Enable verbose output
+```
+
+**Note:**
+    * Pressing Ctrl+C will stop the program only after a file is completely downloaded, in order to avoid saving half-downloaded files. Use -max=<num> to limit number of files
+    * -d implies -v
+    * do not rely on -max-storage or -max-size to save data
+    * I have not tested all combinations of options, you might discover some bugs !!
+
+## Sample Session
+
+```
+$ reddit-rip -sort=top-all -max-size=200 -max=15 r/CoolGuides
+Five Demands, Not One Less. End Police Brutality. [gvf93v].png    [Too Large: 270KB]
+
+Which waters to avoid by region [kxlyl4].jpg    [Done: 189KB]
+
+Marginal Tax [e1w58y].jpg    [Done: 98KB]
+
+Price and service comparison of the biggest shipping orgs in the US. [ibhb4l].jpg    [Done: 121KB]
+
+How Masks And Social Distancing Works [hpamws].jpg    [Done: 111KB]
+
+How paint can change a room [g8up0b].jpg    [Done: 110KB]
+
+The history of confederate flags. [hakuc7].jpg    [Done: 81KB]
+
+How to resist [dg83vg].png    [Too Large: 565KB]
+
+U.S. Flag but each star is scaled proportionally to their state’s population, in roughly it’s geographical position. [kyeej0].jpg    [Done: 158KB]
+
+How gerrymandering works [j0s9j5].jpg    [Done: 60KB]
+
+From the US holocaust museum [gwpq6q].jpg    [Done: 75KB]
+
+How to pack a hiking bag [eqxs03].jpg    [Done: 31KB]
+
+Epicurean paradox [g2axoj].jpg    [Too Large: 259KB]
+
+Mind Fuck Movies [j6x8jl].jpg    [Done: 168KB]
+
+Recognizing a Mentally Abused Brain [j4nd29].png    [Too Large: 282KB]
+
+Units of measurement [iehqe2].jpg    [Done: 65KB]
+
+Geography Terms [fj0h8a].jpg    [Too Large: 728KB]
+
+How untreated ADHD causes and traps you in depression [fu1kf2].jpg    [Done: 161KB]
+
+Copper through the patina process [gzb8y5].jpg    [Done: 96KB]
+
+A Restaurant Guide For How You Want Your Steak Cooked [esxk8v].jpg    [Done: 38KB]
+
+--------------------
+Processed:  21
+Already Downloaded:  0
+Failed:  0
+Saved:  15
+Other:  6
+--------------------
+```
+
+## Another
+
+```
+$ reddit-rip -sort=top-all -min-karma=6000 -max-size=600 -max=20 r/PhysicsMemes
+Island of stability where [jq6t10].gif    [Too Large: 31MB]
+
+made with paint [kncao1].png    [Too Large: 638KB]
+
+I finally saw it irl :D [mkfvna].png    [Too Large: 918KB]
+
+Made during chemistry class. [k5utnz].jpg    [Done: 51KB]
+
+how to thought experiment [l2n9bw].jpg    [Done: 42KB]
+
+Thermal Physics test tomorrow. Wish me luck. [cv5rgt].jpg    [Done: 235KB]
+
+physics major [kjy78w].jpg    [Done: 17KB]
+
+6 marks [imdell].png    [Too Large: 2MB]
+
+Organic Chemistry books are basically a portfolio of hexagons [epu5ep].jpg    [Done: 87KB]
+
+
+Skipping posts with less points, since sort=top-all
+--------------------
+Processed:  10
+Already Downloaded:  0
+Failed:  0
+Saved:  5
+Other:  5
+--------------------
+```
---
+++ dl.go
@@ -0,0 +1,415 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+	"os/signal"
+	"strconv"
+	"strings"
+	"html"
+)
+
+const (
+	UserAgent    = "rip for Reddit / Command Line Tool / Linux"
+	DefaultLimit = 100
+)
+
+type Stats struct {
+	Processed, Saved, Failed, Repeated int
+	CopiedBytes                        int64
+}
+
+// (mostly) command line options
+type Config struct {
+	After, Sort, UserAgent, Folder string
+	Limit, MaxFiles, MinKarma      int
+	Debug, DryRun                  bool
+	MaxStorage, MaxSize            int64
+}
+
+type PostData struct {
+	Url, Name, Title string
+	Ups              int
+}
+
+type Post struct {
+	Data PostData
+}
+
+type ApiData struct {
+	Children []Post
+}
+
+type ApiResponse struct {
+	Data ApiData
+}
+
+// Entire Program is single threaded
+// Passing it around is tedious
+var stats Stats
+
+// similarly for the interrupt channel
+
+var interrupt chan os.Signal
+
+func either(a, b string) string {
+	if a == "" {
+		return b
+	}
+	return a
+}
+
+func fatal(val ...interface{}) {
+	fmt.Println(val...)
+	os.Exit(1)
+}
+
+// unreadable code but saved my keyboard
+func check(e error, extra ...string) {
+	if e != nil {
+		fmt.Println(extra)
+		fatal(e.Error())
+	}
+}
+
+func Log(debug bool, vals ...interface{}) {
+	if debug {
+		fmt.Fprintln(os.Stdout, vals...)
+	}
+}
+
+func size(bytes int64) string {
+	sizes := []int64{1000 * 1000 * 1000, 1000 * 1000, 1000}
+	names := []string{"GB", "MB", "KB"}
+
+	// content length in http response can be -1
+	if (bytes == -1) {
+		return "Unknown length"
+	}
+
+	for i, sz := range sizes {
+		if bytes > sz {
+			units := bytes / sz
+			if (bytes % sz) >= (sz / 2) {
+				units += 1
+			}
+			return strconv.FormatInt(units, 10) + names[i]
+		}
+	}
+	return strconv.FormatInt(bytes, 10) + "B"
+}
+
+func Finish(stats *Stats) {
+	fmt.Println(strings.Repeat("-", 20))
+	fmt.Println("Processed Posts: ", stats.Processed)
+	fmt.Println("Already Downloaded: ", stats.Repeated)
+	fmt.Println("Failed: ", stats.Failed)
+	fmt.Println("Saved: ", stats.Saved)
+	fmt.Println("Other: ",
+		stats.Processed-stats.Failed-stats.Repeated-stats.Saved)
+	fmt.Println(strings.Repeat("-", 20))
+	os.Exit(0)
+}
+
+// body is the response body which contains json
+// handler is run for every post entry unless handler exits early
+// returns last posts's id ('name' attribute in json)
+// which is useful to fetch next page
+func HandlePosts(body io.ReadCloser, handler func(int, PostData)) (last string) {
+	dec := json.NewDecoder(body)
+	apiResponse := ApiResponse{}
+	check(dec.Decode(&apiResponse))
+	for i, post := range apiResponse.Data.Children {
+		stats.Processed += 1
+		handler(i, post.Data)
+		last = post.Data.Name
+	}
+	return last
+}
+
+// Returns whether the image ends with ".jp[e]g", ".png", ".gif" or ".mp4"
+// also the extension string that matched
+func CheckImage(linkString string) (isImage bool, extension string) {
+	var exts = []string{".jpeg", ".gif", ".gifv", ".mp4", ".jpg", ".png"}
+	link, err := url.Parse(linkString)
+	check(err)
+	path := link.Path
+	for _, ext := range exts {
+		if strings.HasSuffix(path, ext) {
+			return true, ext
+		}
+	}
+	return false, ""
+}
+
+// Returns Resp : *http.Response
+func FetchUrl(url string, userAgent string) (*http.Response, error) {
+	req, err := http.NewRequest("GET", url, nil)
+	check(err)
+	req.Header.Add("User-Agent", userAgent)
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	return resp, err
+}
+
+// downloads all images reachable from reddit.com/<path>.json
+func TraversePages(path string, config *Config, handler func(int, PostData)) {
+	target := "https://reddit.com/" + strings.TrimSuffix(path, "/")
+
+	// so that we don't modify at a distance
+	after := config.After
+	// Handle sort options
+	topBy := ""
+	switch config.Sort {
+	case "best", "hot", "new", "rising":
+		target += "/" + config.Sort
+	case "top-hour", "top-day", "top-month", "top-year", "top-all":
+		target += "/top"
+		topBy = strings.TrimPrefix(config.Sort, "top-")
+	case "":
+		_ = "best" // do nothing
+	default:
+		fatal("Invalid option passed to sort")
+	}
+	target += ".json?limit=" + strconv.Itoa(config.Limit)
+	// for sort by top voted
+	if topBy != "" {
+		target += "&t=" + topBy
+	}
+	for {
+		if after != "" {
+			target += "&after=" + after
+		}
+		Log(config.Debug, "REQUEST: ", target)
+		response, err := FetchUrl(target, config.UserAgent)
+		check(err, "Cannot get JSON response")
+
+		processed := stats.Processed
+		after = HandlePosts(response.Body, handler)
+		if stats.Processed == processed {
+			// no items were got in this page
+			Finish(&stats)
+		}
+	}
+}
+
+func DownloadLink(_ int, post PostData, config *Config) {
+	// signal handling:
+	// when ctrl+c is pressed, it should be buffered to a channel
+	// check that after every download
+	// if it's pressed then exit
+
+	select {
+	case sig := <-interrupt:
+		if sig == os.Interrupt {
+			fmt.Println("Interrupt Received, Exit")
+			Finish(&stats)
+		}
+	default:
+		// do nothing
+	}
+	// process title, truncate if too long
+	title := strings.TrimSpace(strings.ReplaceAll(post.Title, "/", "|"))
+	title = html.UnescapeString(title) // &amp; etc.. are escaped in json
+	if len(title) > 194 {
+		title = title[:192] + ".."
+	}
+	// check if Karma limit is met
+	// will any post ever get 2B upvotes?
+	// also: default limit is 1 karma point.
+	if post.Ups < config.MinKarma {
+		Log(config.Debug, "Skipped Due to Less Karma:", title,
+			"| Ups:", post.Ups, "|", post.Url, "\n")
+		fmt.Println()
+		if strings.HasPrefix(config.Sort, "top-") {
+			fmt.Println("Skipping posts with less points, since sort=" + config.Sort)
+			Finish(&stats)
+		}
+		return
+	}
+
+	// check if url is image
+	isImage, extension := CheckImage(post.Url)
+	if !isImage {
+		Log(config.Debug, "Skip non-imagelike entry: ", title, " | ", post.Url, "\n")
+		return
+	}
+	filename := title + " [" + strings.TrimPrefix(post.Name, "t3_") + "]" + extension
+	Log(config.Debug, "URL: ", post.Url, " | Ups:", post.Ups)
+	fmt.Print(filename)
+	// check if already downloaded file
+	_, err := os.Stat(filename)
+	if err == nil {
+		fmt.Println("    [Already Downloaded]")
+		fmt.Println()
+		stats.Repeated += 1
+		return
+	}
+
+	// If dry run, don't fetch media, or create a file
+	// but you still have to increase number of files for config.MaxFiles to work
+	if config.DryRun {
+		fmt.Println("    [Dry Run]")
+		fmt.Println()
+		stats.Saved += 1
+		if stats.Saved == config.MaxFiles {
+			Finish(&stats)
+		}
+		return
+	}
+
+	// CHECK: any edge case?
+	var output *os.File = nil // don't create until needed
+
+	// Common error handling code
+	netError := func(kind string) {
+		stats.Failed += 1
+		fmt.Println("    [" + kind + " Error: " + err.Error() + "]")
+		fmt.Println()
+		if output != nil {
+			output.Close()
+			check(os.Remove(filename))
+		}
+		return
+	}
+	// Fetch
+	response, err := FetchUrl(post.Url, config.UserAgent)
+	if err != nil {
+		netError("Request ")
+		return
+	}
+	length := response.ContentLength
+	if config.MaxSize != -1 && (config.MaxSize < length || length == -1) {
+		fmt.Printf("    [Too Large: %s]\n", size(length))
+		fmt.Println()
+		return
+	}
+	if config.MaxStorage != -1 &&
+			(config.MaxStorage < length+stats.CopiedBytes || length == -1) {
+		fmt.Printf("    [Storage Limit Hit: %s]\n", size(length))
+		fmt.Println()
+		Finish(&stats)
+	}
+
+	// Create file
+	output, err = os.Create(filename)
+	if err != nil {
+		fmt.Println("    [Cannot create file]")
+		fmt.Println()
+		stats.Failed += 1
+		return
+	}
+
+	// Copy
+	n, err := io.Copy(output, response.Body)
+
+	// add n to how much diskspace is consumed even if there's an error
+	// because it would give a more appropriate approximation of bandwidth consumption
+	// But if you're using that option to limit data usage, give 80% of airtime you can use
+	stats.CopiedBytes += n
+
+	if err != nil {
+		netError("Transfer ")
+		response.Body.Close()
+		return
+	}
+
+	// Transfer success I hope
+	// write stats
+	fmt.Printf("    [Done: %s]\n", size(response.ContentLength))
+	fmt.Println()
+	stats.Saved += 1
+	if stats.Saved == config.MaxFiles {
+		Finish(&stats)
+	}
+
+	// close files
+	check(response.Body.Close())
+	check(output.Close())
+	return
+}
+
+func main() {
+	config := Config{}
+	help := false
+	// whether help option is provided
+	flag.BoolVar(&help, "help", false, "Show this help message")
+	// option parsing
+	flag.BoolVar(&config.Debug, "v", false, "Enable verbose output")
+	flag.BoolVar(&config.DryRun, "d", false, "DryRun i.e just print urls and names")
+	flag.StringVar(&config.After, "after", "", "Get posts after the given ID")
+	flag.StringVar(&config.UserAgent, "useragent", UserAgent, "UserAgent string")
+	flag.Int64Var(&config.MaxStorage, "max-storage", -1, "Data usage limit in MB, -1 for no limit")
+	flag.Int64Var(&config.MaxSize, "max-size", -1, "Max size of media file in KB, -1 for no limit")
+	flag.StringVar(&config.Folder, "folder", "", "Target folder name")
+	flag.StringVar(&config.Sort, "sort", "", "Sort: best|hot|new|rising|top-<all|year|month|week|day>")
+	flag.IntVar(&config.MaxFiles, "max", -1, "Max number of files to download (+ve), -1 for no limit")
+	flag.IntVar(&config.MinKarma, "min-karma", 0, "Minimum Karma of the post")
+	flag.IntVar(&config.Limit, "l", 100, "Number of entries to fetch in one API request (devel)")
+	flag.Parse()
+	args := flag.Args()
+	if len(args) != 1 || help {
+		fmt.Fprintf(os.Stdout, "Usage: %s <options> <r/subreddit>\n", os.Args[0])
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	path := args[0]
+
+	// validate some arguments
+	// handling 0 correctly probably requires some more code, so don't
+	toCheck := map[string]int64{
+		"-max":         int64(config.MaxFiles),
+		"-max-storage": config.MaxStorage,
+		"-max-size":    config.MaxSize,
+	}
+	for option, value := range toCheck {
+		if value < 1 && value != -1 {
+			fatal("Invalid value for option " + option)
+		}
+	}
+
+	// enable debug output in case of dry run
+	config.Debug = config.Debug || config.DryRun
+
+	// compute actual MaxStorage in bytes
+	// if you're overflowing this, you have bigger problems
+	if config.MaxStorage != -1 {
+		config.MaxStorage *= 1000 * 1000 // MB
+	}
+
+	if config.MaxSize != -1 {
+		config.MaxSize *= 1000 // KB
+	}
+	// Create folder
+	config.Folder = either(config.Folder,
+		strings.TrimPrefix(strings.ReplaceAll(path, "/", "."), "r."))
+	_, err := os.Stat(config.Folder)
+
+	// Note: not creating folder anew if dry run
+	if os.IsNotExist(err) && !config.DryRun {
+		check(os.MkdirAll(config.Folder, 0755))
+	}
+
+	// if dry run, change to folder only if folder already existed
+	if err == nil || !config.DryRun {
+		check(os.Chdir(config.Folder))
+	}
+
+	// to properly handle Ctrl+C, notify os.Interrupt
+	interrupt = make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+
+	// start downloading
+	TraversePages(path, &config, func(i int, post PostData) {
+		DownloadLink(i, post, &config)
+	})
+}
